8eb8ea458a8d03f3dab49d6bf9a6395c5ba78ca5
meireles-andrade-emf <heloisa.meirelesandrade@studentfr.ch>
2024-09-05 08:52:36 +0200

    ex 3 fini

---
+++ exercices/exercice03/TypesDeBase.java
@@ -0,0 +1,12 @@
+public class TypesDeBase {
+    public static void main(String[] args) {
+        int age = 16;
+        String nombre = "heloisa";
+
+        System.out.println("Je m'appelle " + nombre + " et j'ai " + age + " ans");
+        
+
+    }
+
+
+}
