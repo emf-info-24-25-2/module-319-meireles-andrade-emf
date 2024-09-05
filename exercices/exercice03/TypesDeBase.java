7499cb487ebba14c9bff83f34b6a5163977e0254
meireles-andrade-emf <heloisa.meirelesandrade@studentfr.ch>
2024-09-05 08:53:09 +0200

    ex 3 fini

--- exercices/exercice03/TypesDeBase.java
+++ exercices/exercice03/TypesDeBase.java
@@ -1,12 +1,12 @@
 public class TypesDeBase {
     public static void main(String[] args) {
         int age = 16;
-        String nombre = "heloisa";
+        String nom = "heloisa";
 
-        System.out.println("Je m'appelle " + nombre + " et j'ai " + age + " ans");
+        System.out.println("Je m'appelle " +nom+ " et j'ai " +age+ " ans");
         
+        int sum = age + 10;
 
-    }
+        System.out.println("Dans 10 ans, j'aurai " + sum +" ans");
+    }}
 
-
-}
