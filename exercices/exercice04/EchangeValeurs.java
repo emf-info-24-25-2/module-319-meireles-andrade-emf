0a486d59f2f5e40fc82436c9ba376f25ba7c03a0
meireles-andrade-emf <heloisa.meirelesandrade@studentfr.ch>
2024-09-08 00:00:39 +0200

    ex 04 fini

--- exercices/exercice04/EchangeValeurs.java
+++ exercices/exercice04/EchangeValeurs.java
@@ -2,18 +2,13 @@ public class EchangeValeurs {
     public static void main(String[] args) {
         int variable1 = 1;
         int variable2 = 2;
-       
-
         System.out.println("Le contenu de la variable 1 est " + variable1 );
         System.out.println("Le contenu de la variable 2 est " + variable2);
-        System.out.println("...Traitement...");
-        
+        System.out.println("...Traitement...");   
         variable1 = 2;
         variable2 = 1;
-       
         System.out.println("Le contenu de la variable 1 est " + variable1);
         System.out.println("Le contenu de la variable 1 est " + variable2);
-
         }
     }
 
