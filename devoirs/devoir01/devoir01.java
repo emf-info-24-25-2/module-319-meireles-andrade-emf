10054e7ab409ca02bfb655247f94290a531dae5e
meireles-andrade-emf <heloisa.meirelesandrade@studentfr.ch>
2024-09-09 17:27:05 +0200

    devoir01 fini

---
+++ devoirs/devoir01/devoir01.java
@@ -0,0 +1,16 @@
+public class devoir01 {
+    public static void main(String[] args) {
+        int monAge = 16;
+
+        System.out.println("Je m'appelle Meireles Andrade Heloisa ");
+        System.out.println("Mon age est de : " + monAge + "ans");
+        
+        if (monAge > 20) {
+            System.out.println("Je seus majeur");
+            
+        } else System.out.println("Je suis pas ancore majeur");
+        
+
+        
+    }
+}
---
+++ devoirs/devoir01/devoir1.java
@@ -0,0 +1,16 @@
+package devoir01;
+
+public class devoir01 {
+    public static void main(char[] args) {
+        char monAge = 16;
+        int estMajeur = "non";
+        
+        System.out.println("Je m'appelle Meireles Andrade Heloisa ");
+        System.out.println("Mon age est de : " + monAge + "ans");
+        
+        if (monAge > 20) {System.out.println("Je suis majeur");
+         }
+        else System.out.println("Je suis pas ancore majeur");
+        }
+     }      
+
