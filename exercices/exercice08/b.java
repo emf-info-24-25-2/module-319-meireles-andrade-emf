d4a897c70e8c548fa3959a8070c0c85a590c4df8
meireles-andrade-emf <heloisa.meirelesandrade@studentfr.ch>
2024-09-12 11:44:23 +0200

    exboucles en cours

---
+++ exercices/exercice08/b.java
@@ -0,0 +1,31 @@
+public class b {
+
+    public static void main(String[] args) {
+         
+        //boucle for
+        for(int i = 1; i <= 5; i++ ) {
+                System.out.println("Boulcle for, intération " + i );
+         }
+
+         System.out.println( );
+
+         //boucle while
+         int j = 1;
+         
+         while ( j <= 5) {
+            System.out.println("Boucle while, intération ");
+            j++; 
+         }
+
+         System.out.println( );
+
+         //boucle do-while
+        
+        int k = 1;
+        do {
+            System.out.println("Boucle do-while, itération");
+            k++;
+        }while (k <= 5);
+
+    }
+}
