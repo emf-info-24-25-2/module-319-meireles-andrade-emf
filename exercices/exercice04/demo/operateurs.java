dbf9c6c09a105a9826d37e74f8fd49f3aa927264
meireles-andrade-emf <heloisa.meirelesandrade@studentfr.ch>
2024-09-12 10:20:33 +0200

    ExerciceCondition1 fini

---
+++ exercices/exercice04/demo/operateurs.java
@@ -0,0 +1,64 @@
+public class operateurs {
+    public static void main(String[] args) {
+
+        int operateur = 3;
+        int operateur1 = 3;
+        int operateur2 = 3;
+        int operateur3 = 3;
+        int operateur4 = 3;
+        
+
+        //avant modification
+        System.out.println(operateur);
+        
+        //on applique
+        operateur =2;
+
+        //après modification
+        System.out.println(operateur);
+
+        //avant modification
+        System.out.println(operateur1);
+
+        //on applique
+        operateur1 +=2;
+
+        //après modification
+        System.out.println(operateur1);
+
+        //avant modification
+        System.out.println(operateur2);
+
+        //on applique
+        operateur2-=2;
+
+        //après modification
+        System.out.println(operateur2);
+
+        //avant modification
+        System.out.println(operateur3);
+
+        //on applique
+        operateur3 *=2;
+
+        //après modification
+        System.out.println(operateur3);
+        
+        //avant modification
+        System.out.println(operateur4);
+        
+        //on applique
+        operateur4 /=2;
+
+        //après modification
+        System.out.println(operateur4);
+
+        
+
+        
+
+    
+
+    }
+
+}
