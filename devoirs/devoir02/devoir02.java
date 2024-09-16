359358e286b983659863118a6ee1a50962b5312f
meireles-andrade-emf <heloisa.meirelesandrade@studentfr.ch>
2024-09-16 19:24:45 +0200

    Deivoir 8 fini

---
+++ devoirs/devoir02/devoir02.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class devoir02 {
+
+    public static void main(String[] args) {
+         final int CapacitéReservoirA = 3;
+         final int CapacitéReservoirB = 5;
+
+        System.out.println("Il y a 5 litres à remplir");
+
+         int reservoir1 = 0;
+         int reservoir2 = 0;
+
+        Random random = new random();
+        int remplissage = random.nextInt(CapacitéReservoir1 + CapacitéReservoir2 + 1)
+
+        while (CapacitéReservoirA => 3) {
+                System.out.println("Remplissage du réser");
+        } do 
+        
+    }
+}
---
+++ exercices/exercice07/Operateurs.java
@@ -0,0 +1,60 @@
+public class Operateurs {
+
+    public static void main(String[] args) {
+        int operateur1 = 3;
+        int operateur2 = 3;
+        int operateur3 = 3;
+        int operateur4 = 3;
+        int operateur5 = 3;
+
+        //avant modification
+        System.out.println(operateur1);
+
+        //on applique
+        operateur1 = 2;
+
+        //après modification
+        System.out.println(operateur1);
+
+
+        //avant modification
+        System.out.println(operateur2);
+
+        //on applique
+        operateur2 += 2;
+
+        //après modification
+        System.out.println(operateur2);
+
+        
+        //avant modification
+        System.out.println(operateur3);
+
+        //on applique
+        operateur3 -= 2;
+
+        //après modification
+        System.out.println(operateur3);
+
+        //avant modification
+        System.out.println(operateur4);
+
+        //on applique
+        operateur4 *= 2;
+        
+        //après nodification
+        System.out.println(operateur4);
+
+
+        //avant modification
+        System.out.println(operateur5);
+
+        //on applique
+        operateur5 /= 2;
+
+        //après modification
+        System.out.println(operateur5);
+    }
+
+}
+    
