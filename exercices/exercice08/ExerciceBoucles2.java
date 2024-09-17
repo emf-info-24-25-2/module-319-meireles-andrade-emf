7e5464bdcb0e4c25ef32e0175d5a2f0fb69f43c1
meireles-andrade-emf <heloisa.meirelesandrade@studentfr.ch>
2024-09-17 18:26:45 +0200

    Exercice 8 fini

---
+++ exercices/exercice08/ExerciceBoucles2.java
@@ -0,0 +1,32 @@
+public class ExerciceBoucles2 {
+    public static void main(String[] args) {
+        
+        System.out.println("Boucle for:");
+        for (int i = 5; i >= 1; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Décollage !!");
+        
+        
+        System.out.println();
+        System.out.println("Boucle while:");
+        int j = 5;
+        while (j >= 1) {
+            System.out.println(j);
+            j--;
+        }
+        System.out.println("Décollage !!");
+        
+        
+        System.out.println();
+        System.out.println("Boucle do-while:");
+        int k = 5;
+        do {
+            System.out.println(k);
+            k--;
+        } while (k >= 1);
+        System.out.println("Décollage !!");
+    }
+
+
+}
