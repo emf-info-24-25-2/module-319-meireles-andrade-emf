1eb9afa042e3f62c8c5040af510e1ab49fa88d45
meireles-andrade-emf <heloisa.meirelesandrade@studentfr.ch>
2024-10-31 09:04:46 +0100

    exercice10 methodes

---
+++ exercices/exercice10/exercice10.java
@@ -0,0 +1,25 @@
+public class exercice10 {
+public static void main(String[] args) {
+    
+}
+    
+public void direBonjour () {
+
+    System.out.println("Bonjour");
+}
+
+public void dire(String message) {
+    System.out.println(message);
+}
+
+int minValue = Math.min(10, 20); // minValue vaudra 10
+
+int minValue = Math.min(10, 20); // minValue vaudra 10
+
+double result = Math.pow(2, 3); // result vaudra 8.0 (2^3)
+
+ouble root = Math.sqrt(16); // root vaudra 4.0
+
+int absoluteValue = Math.abs(-5); // absoluteValue vaudra 5
+
+}
