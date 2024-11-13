adde92bf5e1d6e55bebd69a41da6c12333ec3512
Fanny Riedo <141828170+riedo-emf@users.noreply.github.com>
2024-11-13 18:23:41 +0100

    Devoir pas fait

--- devoirs/devoir06/devoir06.java
+++ devoirs/devoir06/devoir06.java
@@ -1,2 +1,4 @@
 /*j'ai essaye de faire le devoir, mais j'ai pas réussi parce que 
 j'ai pas réussi a faire un math random avec les constates*/
+//RIF: la formule de math.random est donnée dans le onenote
+//si vous ne savez pas un point, il faut quand même faire le reste: par exemple vous démarrez avec un tableau de 0
